b6bb39cdeb9b486b6e580df562716fd12c00c7a7
Jonathan <jontopanotti@gmail.com>
2020-09-30 09:57:34 -0300

    User Avatar pt.1

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 
 class User(UserMixin, db.Model):
@@ -12,6 +13,9 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
 
     def __repr__(self):
         return f'<User {self.username}>'
